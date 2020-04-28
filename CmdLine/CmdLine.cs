ca6a1c32b60090a2e8ba87077d21881e72857371
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2020-04-28 21:30:22 +0900

    Add files

---
+++ CmdLine/CmdLine.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CmdLineProcess
+{
+    /// <summary>
+    /// command line process executing class.
+    /// </summary>
+    public class CmdLine : Process
+    {
+        private readonly Action<string> onReceive;
+        private readonly Action<int> onExit;
+        private TaskCompletionSource<int> processExitCode;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CmdLine"/> class.
+        /// </summary>
+        /// <param name="commandName">Command file name.</param>
+        /// <param name="workPath">Work directory.</param>
+        /// <param name="onReceive">Action for recieving output.</param>
+        /// <param name="onExit">Action for after exiting process.</param>
+        public CmdLine(string commandName, string workPath, Action<string> onReceive, Action<int> onExit)
+        {
+            OutputDataReceived += Process_OutputDataReceived;
+            ErrorDataReceived += Process_OutputDataReceived;
+            Exited += Process_Exited;
+            EnableRaisingEvents = true;
+
+            StartInfo = new ProcessStartInfo() {
+                FileName = commandName,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WorkingDirectory = workPath,
+            };
+
+            this.onReceive = onReceive;
+            this.onExit = onExit;
+        }
+
+        /// <summary>
+        /// Start process.
+        /// </summary>
+        /// <param name="args">arguments.</param>
+        public new void Start(string args)
+        {
+            StartInfo.Arguments = args;
+
+            if (Directory.Exists(StartInfo.WorkingDirectory)) {
+                base.Start();
+                BeginOutputReadLine();
+                BeginErrorReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Start process.
+        /// </summary>
+        public new void Start()
+        {
+            Start(string.Empty);
+        }
+
+        /// <summary>
+        /// Start process async.
+        /// </summary>
+        /// <param name="args">arguments.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public virtual async Task<int> StartAsync(string args)
+        {
+            Start(args);
+
+            // Wait Process_Exited is called.
+            processExitCode = new TaskCompletionSource<int>();
+            return await processExitCode.Task.ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Occurs when a process exits.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An object that contains no event data.</param>
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            // Wait output all buffer data.
+            if (!HasExited) {
+                WaitForExit();
+            }
+
+            try {
+                onExit?.Invoke(ExitCode);
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+
+            // Throws "ExitCode" to the waiting task in StartAsync .
+            processExitCode?.SetResult(ExitCode);
+        }
+
+        /// <summary>
+        /// Occurs when output data received.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An object that contains no event data.</param>
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            try {
+                onReceive?.Invoke(e.Data);
+            } catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ CmdLineTests/CmdLineTests.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CmdLineProcess.Tests
+{
+    [TestClass]
+    public class CmdLineTests
+    {
+        private const string MsBuildPath = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe";
+        private readonly string MsBuildVersion = "16.5.0.12403";
+        private readonly ProcessStartInfo expectedPSI = new ProcessStartInfo(){
+            FileName = MsBuildPath,
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = @".\",
+        };
+
+        [TestMethod]
+        public void CmdLineTest()
+        {
+            // Arrange
+            BindingFlags BindingAttr = BindingFlags.NonPublic | BindingFlags.Instance;
+            
+            static void onReceive(string arg)
+            { }
+
+            static void onExit(int arg)
+            { }
+
+            // Act
+            var testClass = new CmdLine(
+                expectedPSI.FileName,
+                expectedPSI.WorkingDirectory,
+                onReceive,
+                onExit
+                );
+
+            // Assert
+            var receiveAction = (Action<string>)testClass.GetType().GetField("onReceive", BindingAttr).GetValue(testClass);
+            var exitAction = (Action<int>)testClass.GetType().GetField("onExit", BindingAttr).GetValue(testClass);
+
+            Assert.AreEqual(true, testClass.EnableRaisingEvents);
+            Assert.AreEqual(expectedPSI.FileName, testClass.StartInfo.FileName);
+            Assert.AreEqual(expectedPSI.CreateNoWindow, testClass.StartInfo.CreateNoWindow);
+            Assert.AreEqual(expectedPSI.UseShellExecute, testClass.StartInfo.UseShellExecute);
+            Assert.AreEqual(expectedPSI.RedirectStandardOutput, testClass.StartInfo.RedirectStandardOutput);
+            Assert.AreEqual(expectedPSI.RedirectStandardError, testClass.StartInfo.RedirectStandardError);
+            Assert.AreEqual(expectedPSI.WorkingDirectory, testClass.StartInfo.WorkingDirectory);
+            Assert.AreEqual(onExit, exitAction);
+            Assert.AreEqual(onReceive, receiveAction);
+        }
+
+        [TestMethod()]
+        public void StartTestWithArg()
+        {
+            // Arrange
+            var outstr = string.Empty;
+            var retcode = 0;
+
+            void onReceive(string arg)
+            {
+                outstr += arg + Environment.NewLine;
+            }
+
+            void onExit(int arg)
+            {
+                retcode = arg;
+            }
+
+            // Act
+            var testClass = new CmdLine(
+                expectedPSI.FileName,
+                expectedPSI.WorkingDirectory,
+                onReceive,
+                onExit
+                );
+
+            // Act
+            testClass.Start(@"-ver");
+            testClass.WaitForExit();
+            
+            // Assert
+            Assert.AreEqual(retcode, 0);
+            Assert.IsTrue(outstr.Contains(MsBuildVersion));
+        }
+
+        [TestMethod()]
+        public void StartTestArg()
+        {
+            // Arrange
+            var retcode = 0;
+
+            // Act
+            var testClass = new CmdLine(
+                expectedPSI.FileName,
+                expectedPSI.WorkingDirectory,
+                new Action<string>((arg) => { }),
+                new Action<int>((arg) => { })
+                );
+
+            // Act
+            testClass.Start();
+            testClass.WaitForExit();
+
+            // Assert
+            Assert.AreEqual(retcode, 0);
+        }
+
+        [TestMethod()]
+        public void StartTestHandlerNull()
+        {
+            // Arrange
+            var retcode = 0;
+
+            // Act
+            var testClass = new CmdLine(expectedPSI.FileName, expectedPSI.WorkingDirectory, null, null);
+
+            // Act
+            testClass.Start();
+            testClass.WaitForExit();
+
+            // Assert
+            Assert.AreEqual(retcode, 0);
+        }
+
+        [TestMethod()]
+        public async Task StartAsyncTestAsync()
+        {
+            // Arrange
+            var outstr = string.Empty;
+            var retcode = 0;
+
+            void onReceive(string arg)
+            {
+                outstr += arg + Environment.NewLine;
+            }
+
+            void onExit(int arg)
+            {
+                retcode = arg;
+            }
+
+            // Act
+            var testClass = new CmdLine(
+                expectedPSI.FileName,
+                expectedPSI.WorkingDirectory,
+                onReceive,
+                onExit
+                );
+
+            // Act
+            var actualRetCode = await testClass.StartAsync(@"-ver");
+
+            // Assert
+            Assert.AreEqual(retcode, actualRetCode);
+            Assert.IsTrue(outstr.Contains(MsBuildVersion));
+        }
+    }
+}
